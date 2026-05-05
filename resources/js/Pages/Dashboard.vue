c5b5ef8d096fd2514958d85806724f71619f29ec
Rogalex ZANNOU <rogalex.zannou@chem.loc>
2026-05-05 12:28:59 +0200

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted } from 'vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { useToast } from 'primevue/usetoast';
+import Toast from 'primevue/toast';
+const toast = useToast();
+
+onMounted( () =>{
+    toast.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 })
+});
+
+
+
+</script>
+
+<template>
+    <Head title="Dashboard" />
+    <AuthenticatedLayout>
+        <template #header>
+            <Toast />
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        You're logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
