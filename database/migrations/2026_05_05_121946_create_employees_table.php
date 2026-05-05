fe235eebc3a2bd94b5ec564f7c5c9cbecc9289a5
Otniel SECHEGBE <otniel.sechegbe@chem.loc>
2026-05-05 13:23:17 +0100

    migrations_emp/pos

---
+++ database/migrations/2026_05_05_121946_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+        $table->string('matricule')->unique();
+        $table->string('first_name');
+        $table->string('last_name');
+        $table->date('birth_date')->nullable();
+        $table->string('phone')->nullable();
+        $table->string('email')->nullable();
+        $table->text('address')->nullable();
+        $table->foreignId('position_id')->constrained();
+        $table->decimal('salary_base', 10, 2)->default(0);
+        $table->string('status')->default('actif');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
